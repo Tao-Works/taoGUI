73cafa008fee4eb5c57c8dd2ec4a9a930112d4e8
plus33 <plus33@gmx.net>
2017-02-01 01:16:55 +0100

    * Added Test XL-Files to repository -> Unit tests should all work now

--- taoGUI_UnitTest/Caching/TaoStatAccessor_Test.cs
+++ taoGUI_UnitTest/Caching/TaoStatAccessor_Test.cs
@@ -11,17 +11,18 @@ namespace taoGUI_UnitTest.Caching {
 
   [TestClass]
   public class TaoStatAccessor_Test {
+    private static string DIR_Test_Data = @"..\..\Test_Data";
 
     [TestMethod]
     public void Test_TaoStatAccessor_NO_Cache() {
-      TaoStatAccessorI statAccessor = TaoAccessorFactory.getStatAccessor(AccessType.NoCache, ".", "appId");
+      TaoStatAccessorI statAccessor = TaoAccessorFactory.getStatAccessor(AccessType.NoCache, DIR_Test_Data, "appId");
       TaoStatisticVo taoStats = statAccessor.getStats("A_TaoReport", "dev");
       Assert.IsNotNull(taoStats);
     }
 
     [TestMethod]
     public void Test_TaoStatAccessor_WITH_Cache() {
-      TaoStatAccessorI statAccessor = TaoAccessorFactory.getStatAccessor(AccessType.WithCache, ".", "appId");
+      TaoStatAccessorI statAccessor = TaoAccessorFactory.getStatAccessor(AccessType.WithCache, DIR_Test_Data, "appId");
       TaoStatisticVo taoStats = statAccessor.getStats("A_TaoReport", "dev");
       Assert.IsNotNull(taoStats);
     }
